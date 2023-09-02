7a4a81d17320f2af78ac84009e24cded6be4b6aa
seehar <seehar@users.noreply.github.com>
2023-09-02 23:19:44 +0800

    deploy: 583357fb21a6973039170960a04a457e78fa46c1

---
+++ assets/guide_installation.md.8acd3a86.lean.js
@@ -0,0 +1,5 @@
+import{_ as s,o as a,c as e,S as n,k as l}from"./chunks/framework.c916636c.js";const F=JSON.parse('{"title":"安装","description":"","frontmatter":{"title":"安装","lang":"zh-CN"},"headers":[],"relativePath":"guide/installation.md","filePath":"guide/installation.md","lastUpdated":1693667944000}'),p={name:"guide/installation.md"},t=n("",20),r=l("iframe",{height:"400",style:{width:"100%"},scrolling:"no",title:"Untitled",src:"https://codepen.io/seehar/embed/rNoLXPX?default-tab=html%2Cresult",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/seehar/pen/rNoLXPX">
+  Untitled</a> by seehar (<a href="https://codepen.io/seehar">@seehar</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),o=[t,r];function c(i,d,h,D,y,u){return a(),e("div",null,o)}const b=s(p,[["render",c]]);export{F as __pageData,b as default};
