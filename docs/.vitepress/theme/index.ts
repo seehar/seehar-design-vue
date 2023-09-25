32cc21f11e93c6aad5ef889ff35953a7e91f4fdd
seehar <seehar@qq.com>
2023-09-25 10:12:32 +0800

    fix: 修复打包错误

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,14 +4,15 @@
  * @Description: 主题配置
  */
 import DefaultTheme from 'vitepress/theme'
-import SeeharDesign from '../../../packages/index' // 本地开发
+// import SeeharDesign from '../../../packages/index' // 本地开发
 // import SeeharDesign from 'seehar-design-vue'
 // import 'seehar-design-vue/lib/style.css'
 import '../vitepress/style/tailwind.css'
 
 import { VPDemo } from '../vitepress'
+import type { EnhanceAppContext } from "vitepress";
 
-// console.log(import.meta.env)
+console.log(import.meta.env)
 console.log(
   '%c seehar love tt forever ',
   'padding: 1px; border-radius: 3px; color: #fff; background: #ff2d51'
@@ -31,9 +32,34 @@ console.log(
 
 export default {
   extends: DefaultTheme, // 默认主题
-  enhanceApp(ctx) {
-    // 注册全局组件
-    ctx.app.use(SeeharDesign)
-    ctx.app.component('demo', VPDemo)
+  enhanceApp: async (ctx: EnhanceAppContext) => {
+    const {app} = ctx
+    app.component("demo", VPDemo)
+    DefaultTheme.enhanceApp(ctx)
+    console.log(import.meta.env);
+    // const module = await import("../../../packages/index")
+    // import("../../../packages/assets/icon/iconfont.js")
+
+    // const module = await import("../../../lib/seehar-design-vue.mjs")
+    // import("../../../lib/assets/icon/iconfont.js")
+    // app.use(module.default)
+
+    const module = await import("seehar-design-vue")
+    // @ts-ignore
+    // import 'seehar-design-vue/lib/style.css'
+    import("seehar-design-vue/lib/style.css")
+    import("seehar-design-vue/packages/assets/icon/iconfont.js")
+    app.use(module.default)
+
+    // if (import.meta.env.MODE === "development") {
+    //   const module = await import("../../../packages/index")
+    //   import("../../../packages/assets/icon/iconfont.js")
+    //   app.use(module.default)
+    // } else {
+    //   const module = await import("seehar-design-vue")
+    //   // @ts-ignore
+    //   import 'seehar-design-vue/lib/style.css'
+    //   app.use(module.default)
+    // }
   }
 }
